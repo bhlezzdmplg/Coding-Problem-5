5e8c265cf60d71f8000353a63c6805b8e0ceafa1
bhlezzdmplg <yharrasallyguiao@gmail.com>
2024-11-05 10:47:16 +0800

    Create colorPreferences.js

---
+++ colorPreferences.js
@@ -0,0 +1,10 @@
+let colors = [];
+
+colors.push(prompt("Enter your first favorite color: "));
+console.log(Purple);
+
+colors.push(prompt("Enter your second favorite color: "));
+console.log(Lavender);
+
+colors.push(prompt("Enter your third favorite color: "));
+console.log(White);
